6ebe9428c1dff8b84bc3c9e43fa5e1e17b6ec222
Robby Ardison <101854553+robbieardison@users.noreply.github.com>
2023-10-07 21:16:40 +0700

    added alert to password

--- gui_control.py
+++ gui_control.py
@@ -2,8 +2,21 @@ import pyautogui
 import keyboard
 import time
 
-pyautogui.moveTo(100, 500, duratio=1)
+# pyautogui.moveTo(100, 500, duration=1)
 
-time.sleep(2) # 2 seconds
+# time.sleep(2) # 2 seconds
 
-pyautogui.moveTo(500, None, duration=1.5) # uses the same Y as before
+pyautogui.moveTo(410, 560, duration=1.5) # uses the same Y as before
+
+pyautogui.click()
+
+# pyautogui.typewrite("Hello, world!", interval=0.2)
+# pyautogui.press('enter')
+# pyautogui.keyDown('shift')
+# pyautogui.write('uppercase')
+# pyautogui.keyUp('shift')
+
+print(pyautogui.alert(text="text", title="title", button="ok"))
+print(pyautogui.confirm(text='text', title='title', buttons=['OK','CANCEL'])) # returns text of button clicked on
+print(pyautogui.prompt(text='text', title='title', default='default')) # returns input text
+print(pyautogui.password(text='text', title='title', default='default', mask='*'))
